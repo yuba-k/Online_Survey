abd6c4429abfc6069d33b659e66c432c88429d3f
murayu427 <murayu0427@gmail.com>
2026-06-22 14:38:58 +0900

    利用規約とプライバシーポリシーのリンク追加

--- php/footer.php
+++ php/footer.php
@@ -2,9 +2,9 @@
 echo "
     <footer>
         <div>
-            <a href=''>利用規約</a>
-            <a href=''>プライバシーポリシー</a>
+            <a href='term.php'>利用規約</a>
+            <a href='privacy.php'>プライバシーポリシー</a>
         </div>
-        <p>&copy2026 村上製作所 All Rights Reserved.</p>
+        <p>&copy;2026 村上製作所 All Rights Reserved.</p>
     </footer>
     ";
